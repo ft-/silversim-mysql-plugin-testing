525e69617738d13fefa092837aef02e2ab29f4ee
ft <ft@noemail>
2017-09-21 21:35:54 +0200

    added IInventoryItemServiceInterface.Move

--- SilverSim/Database.MySQL/Inventory/MySQLInventoryService.Items.cs
+++ SilverSim/Database.MySQL/Inventory/MySQLInventoryService.Items.cs
@@ -302,6 +302,11 @@ namespace SilverSim.Database.MySQL.Inventory
             IncrementVersion(principalID, toFolderID);
         }
 
+        void IInventoryItemServiceInterface.Copy(UUID principalID, UUID id, UUID newFolder)
+        {
+            CopyItem(principalID, id, newFolder);
+        }
+
         private void IncrementVersion(UUID principalID, UUID folderID)
         {
             try
