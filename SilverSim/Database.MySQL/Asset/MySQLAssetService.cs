d0bccc5033075459bfb2b5c8f0f3500e211bfbeb
ft <ft@noemail>
2018-06-14 11:43:18 +0200

    add new override for length requesting TryGetValue

--- SilverSim/Database.MySQL/Asset/MySQLAssetService.cs
+++ SilverSim/Database.MySQL/Asset/MySQLAssetService.cs
@@ -207,6 +207,48 @@ namespace SilverSim.Database.MySQL.Asset
             }
         }
 
+        public override bool TryGetValue(UUID key, out AssetMetadata metadata, out int length)
+        {
+            metadata = null;
+            length = 0;
+            using (var conn = new MySqlConnection(m_ConnectionString))
+            {
+                conn.Open();
+                using (var cmd = new MySqlCommand("SELECT id, LENGTH(data) AS dataLength, assetType, name, create_time, access_time, asset_flags, temporary FROM assetrefs INNER JOIN assetdata ON assetrefs.hash = assetdata.hash AND assetrefs.assetType = assetdata.assetType WHERE id = @id LIMIT 1", conn))
+                {
+                    cmd.Parameters.AddParameter("@id", key);
+                    using (MySqlDataReader dbReader = cmd.ExecuteReader())
+                    {
+                        if (!dbReader.Read())
+                        {
+                            return false;
+                        }
+                        metadata = new AssetMetadata
+                        {
+                            ID = dbReader.GetUUID("id"),
+                            Type = dbReader.GetEnum<AssetType>("assetType"),
+                            Name = dbReader.GetString("name"),
+                            CreateTime = dbReader.GetDate("create_time"),
+                            AccessTime = dbReader.GetDate("access_time"),
+                            Flags = dbReader.GetEnum<AssetFlags>("asset_flags"),
+                            Temporary = dbReader.GetBool("temporary")
+                        };
+                        length = dbReader.GetInt32("dataLength");
+                    }
+                }
+                if (DateTime.UtcNow - metadata.AccessTime > TimeSpan.FromHours(1))
+                {
+                    /* update access_time */
+                    using (var cmd = new MySqlCommand("UPDATE assetrefs SET access_time = @access WHERE id = @id", conn))
+                    {
+                        cmd.Parameters.AddWithValue("@access", Date.GetUnixTime());
+                        cmd.Parameters.AddWithValue("@id", key);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                return true;
+            }
+        }
         #endregion
 
         #region Metadata interface
